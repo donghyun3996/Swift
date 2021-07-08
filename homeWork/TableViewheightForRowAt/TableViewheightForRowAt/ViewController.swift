73ca86f130db0d9fbd8e18debb7d3ebbe325b6c6
donghyun3996 <81354005+donghyun3996@users.noreply.github.com>
2021-07-08 21:11:26 +0900

    Update ViewController.swift

--- homeWork/TableViewheightForRowAt/TableViewheightForRowAt/ViewController.swift
+++ homeWork/TableViewheightForRowAt/TableViewheightForRowAt/ViewController.swift
@@ -5,46 +5,59 @@ class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
     var str = ["조유리","김채원","최예나","김민주","안유진"]
     var image = ["jyr1.jpeg","kcw.jpeg","cyn.jpeg","kmj.jpeg","ayj.jpeg"]
     var textColor = [UIColor.orange,UIColor.green,UIColor.yellow,UIColor.gray,UIColor.blue]
+
     
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    func numberOfSections(in tableView: UITableView) -> Int {
         return str.count
     }
     
-   
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let header = UIView()
+        header.backgroundColor = UIColor.white
+        return header
+    }
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "tableViewCell", for: indexPath)
-        cell.textLabel?.text = self.str[indexPath.row]
-        cell.imageView?.image = UIImage(named: image[(indexPath as NSIndexPath).row])
-        cell.textLabel?.textColor = textColor[indexPath.row]
+        cell.textLabel?.text = self.str[indexPath.section]
+        cell.imageView?.image = UIImage(named: image[(indexPath as NSIndexPath).section])
+        cell.textLabel?.textColor = textColor[indexPath.section]
         cell.backgroundColor = UIColor.white
-                cell.layer.borderColor = UIColor.black.cgColor
-        
-                cell.layer.borderWidth = 1
-                cell.layer.cornerRadius = 8
-                cell.clipsToBounds = true
+        cell.layer.shadowOpacity = 0.3
+        cell.layer.borderWidth = 1
+        cell.layer.cornerRadius = 6
         
         return cell
     }
 
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 40
+    }
+    
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 170
     }
     
     
+
     @IBOutlet var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         tableView.dataSource = self
         tableView.delegate = self
-        
+        tableView.separatorStyle = UITableViewCell.SeparatorStyle.none
     }
 
-
+    
 }
-
 
 
