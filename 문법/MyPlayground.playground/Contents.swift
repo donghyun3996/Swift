bad5f7fbd105c5204f3ff711792ead8e21a74c09
이동현 <81354005+donghyun3996@users.noreply.github.com>
2021-10-25 22:51:25 +0900

    Update Contents.swift

--- 문법/MyPlayground.playground/Contents.swift
+++ 문법/MyPlayground.playground/Contents.swift
@@ -1,3 +1,24 @@
 import UIKit
 
-class User
+class User{
+    var nickname: String
+    var age: Int
+    
+    init(nickname:String, age: Int)
+    {
+        self.nickname = nickname
+        self.age = age
+    }
+    init(age:Int){
+        self.nickname = "donghun"
+        self.age = age
+    }
+}
+var user = User(nickname: "donhyun", age: 17)
+user.nickname
+user.age
+
+
+var user2 = User(age: 18)
+user2.nickname
+user2.age
