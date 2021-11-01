76bb98813688e38c512c290b182f8e86a740bf73
이동현 <81354005+donghyun3996@users.noreply.github.com>
2021-11-01 20:44:20 +0900

    Delete FastCampus/Basic/BasicWiseSaying directory

--- FastCampus/Basic/BasicWiseSaying/BasicWiseSaying/WiseSayin.swift
+++
@@ -1,8 +0,0 @@
-//
-//  WiseSayin.swift
-//  BasicWiseSaying
-//
-//  Created by GSM07 on 2021/09/23.
-//
-
-import Foundation
--- FastCampus/Basic/BasicWiseSaying/Pods/Target Support Files/Pods-BasicWiseSaying/Pods-BasicWiseSaying-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_BasicWiseSaying : NSObject
-@end
-@implementation PodsDummy_Pods_BasicWiseSaying
-@end
