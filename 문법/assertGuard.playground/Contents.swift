e1304ba3571ac45904414069c130a526e8baed2c
이동현 <81354005+donghyun3996@users.noreply.github.com>
2021-11-04 20:33:50 +0900

    Add files via upload

---
+++ 문법/assertGuard.playground/Contents.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+var value = 0
+assert(value == 0)
+
+value = 2
+assert(value == 0,"값이 0이 아닙니다")
+
+
+func guardTest(value: Int?){
+    guard let value = value else {
+        return
+    }
+}
+
+guardTest(value: 2)
+guardTest(value: nil)
